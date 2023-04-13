6ead6d9e960781a875653b40c24805ed60e41d08
ZXYY888 <130648614+ZXYY888@users.noreply.github.com>
2023-04-14 06:26:46 +0800

    Update and rename handlers/handler.go to 处理器/handler.go

--- 处理器/handler.go
+++ 处理器/handler.go
@@ -13,7 +13,7 @@ import (
 	"time"
 )
 
-const deadlineExceededText = "请求GPT服务器超时[裂开]得不到回复，请重新发送问题[旺柴]"
+const deadlineExceededText = "v我50继续聊天[旺柴]"
 
 var c = cache.New(config.LoadConfig().SessionTimeout, time.Minute*5)
 
